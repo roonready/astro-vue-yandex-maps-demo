3977d09060bb2145cf7b41754ad00defd5156795
Sergei Revkov <akkuru@yandex.ru>
2024-02-17 21:21:19 +0300

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [vue({ appEntrypoint: '/src/lib/mapini' })], vite: {
-        ssr: {
-          noExternal: [
-            'vue-yandex-maps',
-                      ]
-        }
-      },
-      output: 'server',
+  integrations: [vue({ appEntrypoint: '/src/lib/mapini' })], vite: {
+    ssr: {
+      noExternal: [
+        'vue-yandex-maps',
+      ]
+    }
+  },
+  output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
